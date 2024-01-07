5ecb07c785a6917c5889f0248cc16661f9948dca
ivanklaric <iklaric@gmail.com>
2024-01-07 11:05:30 +0100

    more methods content

--- playground/tourofgo/methods.go
+++ playground/tourofgo/methods.go
@@ -13,7 +13,24 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+func Abs2(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func funScale(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
 func main() {
 	v := Vertex{3, 4}
 	fmt.Println(v.Abs())
+	v.Scale(10)
+	funScale(&v, 2)
+	fmt.Println(Abs2(v))
 }
