4a9d962d13fe15353b0295093f9f7a009114b14b
Ivan Klaric <iklaric@gmail.com>
2024-01-15 18:43:59 +0100

    Added wikidata toolkit piping into the mix.

---
+++ playground/wikidata/importer/src/main/java/dev/klax/wikidata/importer/commands/WikiDataImporter.java
@@ -0,0 +1,37 @@
+package dev.klax.wikidata.importer.commands;
+
+import dev.klax.wikidata.importer.wdtkutils.CustomEntityProcessor;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.wikidata.wdtk.dumpfiles.DumpContentType;
+import org.wikidata.wdtk.dumpfiles.DumpProcessingController;
+import org.wikidata.wdtk.dumpfiles.EntityTimerProcessor;
+import org.wikidata.wdtk.dumpfiles.MwLocalDumpFile;
+
+
+
+
+@ShellComponent
+public class WikiDataImporter {
+
+
+    @ShellMethod(key = "import")
+    public String importWikidata(String filename) {
+        var dumpProcessingController = new DumpProcessingController("wikidatawiki");
+        var dumpFile = new MwLocalDumpFile(filename, DumpContentType.JSON, "latest", "wikidatawiki");
+        var entityProcessor = new CustomEntityProcessor();
+        dumpProcessingController.registerEntityDocumentProcessor(entityProcessor, null, true);
+
+        var entityTimerProcessor = new EntityTimerProcessor(20);
+        dumpProcessingController.registerEntityDocumentProcessor(entityTimerProcessor, null, true);
+
+        try {
+            dumpProcessingController.processDump(dumpFile);
+        } catch (EntityTimerProcessor.TimeoutException te) {
+            entityTimerProcessor.close();
+        }
+
+        return "Processed " + Integer.toString(entityProcessor.getItemDocsCount()) +" items and "
+                + Integer.toString(entityProcessor.getPropertyDocsCount()) + " properties.";
+    }
+}
---
+++ playground/wikidata/importer/src/main/java/dev/klax/wikidata/importer/wdtkutils/CustomEntityProcessor.java
@@ -0,0 +1,34 @@
+package dev.klax.wikidata.importer.wdtkutils;
+
+import org.wikidata.wdtk.datamodel.interfaces.EntityDocumentProcessor;
+import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
+import org.wikidata.wdtk.datamodel.interfaces.PropertyDocument;
+
+public class CustomEntityProcessor implements EntityDocumentProcessor {
+
+    private int itemDocsCount = 0;
+    private int propertyDocsCount = 0;
+
+    @Override
+    public void processItemDocument(ItemDocument itemDoc) {
+        itemDocsCount++;
+    }
+
+    @Override
+    public void processPropertyDocument(PropertyDocument propDoc) {
+        propertyDocsCount++;
+    }
+
+
+    public int getItemDocsCount() {
+        return itemDocsCount;
+    }
+
+    public void setItemDocsCount(int itemDocsCount) {
+        this.itemDocsCount = itemDocsCount;
+    }
+
+    public int getPropertyDocsCount() {
+        return propertyDocsCount;
+    }
+}
