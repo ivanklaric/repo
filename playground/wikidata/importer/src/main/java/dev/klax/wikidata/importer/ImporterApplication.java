0dd04784d33ac6da1a2ab6a01757e48b67691e7e
Ivan Klaric <iklaric@gmail.com>
2024-01-24 08:20:55 +0100

    Added persistance. This broke tests but tests weren't that well written anyhow, I should test layer by layer. Anyhow, happy enough with this to declare first victory.

--- playground/wikidata/importer/src/main/java/dev/klax/wikidata/importer/ImporterApplication.java
+++ playground/wikidata/importer/src/main/java/dev/klax/wikidata/importer/ImporterApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
 @SpringBootApplication
 @ComponentScan(basePackages = {"dev.klax.sports", "dev.klax.wikidata"})
 @EnableJpaRepositories("dev.klax.sports.repository")
