5a0cb9162e0af22c934155b6a4c3704c111b748d
Ivan Klaric <iklaric@gmail.com>
2024-01-28 21:06:01 +0100

    Linter complaint.

--- playground/wikidata/importer/src/main/java/dev/klax/sports/repository/SportsClubRepository.java
+++ playground/wikidata/importer/src/main/java/dev/klax/sports/repository/SportsClubRepository.java
@@ -1,6 +1,5 @@
 package dev.klax.sports.repository;
 
-import dev.klax.sports.datamodel.Sport;
 import dev.klax.sports.datamodel.SportsClub;
 import org.springframework.data.repository.CrudRepository;
 
