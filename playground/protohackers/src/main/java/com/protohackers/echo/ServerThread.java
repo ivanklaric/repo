41dfb0ee46df39f120cfa1a56274241a52887f48
Ivan Klaric <iklaric@gmail.com>
2024-02-03 09:34:32 +0100

    Addressed linter complaints.

--- playground/protohackers/src/main/java/com/protohackers/echo/ServerThread.java
+++ playground/protohackers/src/main/java/com/protohackers/echo/ServerThread.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.net.Socket;
 
 public class ServerThread extends Thread {
-    private Socket socket;
+    private final Socket socket;
 
     public ServerThread(Socket socket) {
         this.socket = socket;
