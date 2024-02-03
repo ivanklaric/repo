26ff577d89aeb429b1254b3ff8a63aa40a9ff96b
Ivan Klaric <iklaric@gmail.com>
2024-02-03 09:34:07 +0100

    Addressed linter complaints.

--- playground/protohackers/src/main/java/com/protohackers/echo/EchoService.java
+++ playground/protohackers/src/main/java/com/protohackers/echo/EchoService.java
@@ -2,26 +2,22 @@ package com.protohackers.echo;
 
 import java.io.*;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class EchoService {
     public static void main(String[] args) {
         int port = 9003; // TODO: this should come from command line args
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(port);
-        } catch (IOException e) {
-            System.out.println("Error binding to a port: " + e);
-            return;
-        }
 
-        while (true) {
-            try {
-                new ServerThread(serverSocket.accept()).start();
-            } catch (IOException e) {
-                System.out.println("Error creating a socket: " + e);
-                return;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try {
+                    new ServerThread(serverSocket.accept()).start();
+                } catch (IOException e) {
+                    System.out.println("Error creating a socket: " + e);
+                    return;
+                }
             }
+        } catch (IOException e) {
+            System.out.println("Error when opening server: " + e);
         }
     }
 }
