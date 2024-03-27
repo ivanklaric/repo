6967265780a5f5ccecbde2b49a3d5d6f3e8f2fd6
Ivan Klaric <iklaric@gmail.com>
2024-03-27 19:43:49 +0100

    Added basic support for running non-TCP apps in the main class.

--- playground/protohackers/src/main/java/com/protohackers/Main.java
+++ playground/protohackers/src/main/java/com/protohackers/Main.java
@@ -26,25 +26,12 @@ public class Main {
         return null;
     }
 
-    public static void main(String[] args) {
-        int port = 9003; // TODO: this should come from command line args
-
-        System.out.println("Listening on " + port);
-        String[] listOfAcceptableArgs = {"echo", "prime", "means", "budget"};
-        Set<String> acceptableArgs = new HashSet<>(Arrays.asList(listOfAcceptableArgs));
-        if (args.length < 1 || !acceptableArgs.contains(args[0])) {
-            System.out.println("Args:");
-            System.out.println("echo  - for EchoService");
-            System.out.println("prime - for PrimeTime");
-            System.out.println("means - for MeansToAnEnd");
-            System.out.println("budget - for BudgetChat");
-            return;
-        }
+    public static void runSocketApps(String appName, int port) {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
                 try {
                     var socket = serverSocket.accept();
-                    var thread = buildNewServerThread(args[0], socket);
+                    var thread = buildNewServerThread(appName, socket);
                     if (thread != null)
                         thread.start();
                 } catch (IOException e) {
@@ -54,6 +41,34 @@ public class Main {
             }
         } catch (IOException e) {
             System.out.println("Error when opening server: " + e);
+        }
+    }
+
+    public static void runUdpApps(String appName, int port) {
+        System.out.println("Implement me");
+    }
+
+    public static void main(String[] args) {
+        int port = 9003; // TODO: this should come from command line args
+
+        System.out.println("Listening on " + port);
+        String[] listOfAcceptableArgs = {"echo", "prime", "means", "budget", "unusual-server", "unusual-client"};
+        Set<String> acceptableArgs = new HashSet<>(Arrays.asList(listOfAcceptableArgs));
+        String appName = args[0];
+        if (args.length < 1 || !acceptableArgs.contains(appName)) {
+            System.out.println("Args:");
+            System.out.println("echo  - for EchoService");
+            System.out.println("prime - for PrimeTime");
+            System.out.println("means - for MeansToAnEnd");
+            System.out.println("budget - for BudgetChat");
+            System.out.println("unusual-server - for UnusualDatabaseProgram server");
+            System.out.println("unusual-client - for UnusualDatabaseProgram client");
+            return;
+        }
+        if (!appName.equals("unusual")) {
+            runSocketApps(appName, port);
+        } else {
+            runUdpApps(appName, port);
         }
     }
 
